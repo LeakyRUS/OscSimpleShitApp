b51a16c9a282157c85288353a90e2357f9f5ca8c
LeakyRUS <20557088+LeakyRUS@users.noreply.github.com>
2025-01-04 14:10:37 +0300

    Media control improve

--- OscSimpleShitApp/PatternHandlers/MediaPatternHandler.cs
+++ OscSimpleShitApp/PatternHandlers/MediaPatternHandler.cs
@@ -8,7 +8,7 @@ public class MediaPatternHandler : IPatternHandler
     private static readonly GlobalSystemMediaTransportControlsSessionManager _manager =
         GlobalSystemMediaTransportControlsSessionManager.RequestAsync().GetAwaiter().GetResult();
 
-    public string Replace(string parameter)
+    private string GetMediaInfo(bool showTime)
     {
         var cs = _manager.GetCurrentSession();
         if (cs == null)
@@ -20,11 +20,24 @@ public class MediaPatternHandler : IPatternHandler
 
         var str = $"{pi.PlaybackStatus}: {mp.Artist} - {mp.Title}";
 
-        if (tp.EndTime != TimeSpan.Zero)
+        if (tp.EndTime != TimeSpan.Zero && showTime)
         {
             str += $"\n{tp.Position:mm\\:ss} : {tp.EndTime:mm\\:ss}";
         }
 
         return str;
+    }
+
+    public string Replace(string parameter)
+    {
+        var parameters = parameter.Split('|');
+        var showTime = true;
+
+        if (parameters.Length > 0 && bool.TryParse(parameters[0], out var hasShowTime))
+        {
+            showTime = hasShowTime;
+        }
+
+        return GetMediaInfo(showTime);
     }
 }
